8c90574d3b410dd53e6cde8b2248a220c055f231
pardana <aditria.pardana@yahoo.co.id>
2024-07-30 16:31:12 +0700

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { Text } from "react-native";
+import { Slot } from "expo-router";
+
+const RootLayout = () => {
+  return (
+    <>
+      <Text>Header</Text>
+      <Slot />
+      <Text>Footer</Text>
+    </>
+  );
+};
+
+export default RootLayout;
