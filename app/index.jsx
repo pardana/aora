ddcaf67e7db3ec19e467aef137248e2828b596d7
pardana <aditria.pardana@yahoo.co.id>
2024-07-31 13:38:39 +0700

    add: profile and stack screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text>Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href={"/profile"} style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
     </View>
   );
 };
