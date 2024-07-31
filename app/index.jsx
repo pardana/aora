851fc79085ad6554cf28918ab2046d4d752bb3b8
pardana <aditria.pardana@yahoo.co.id>
2024-07-31 14:22:01 +0700

    add: config tailwind and fonts images

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className="flex-1 items-center justify-center bg-grey-500">
+      <Text className="text-3xl font-bold">Aora!</Text>
       <StatusBar style="auto" />
       <Link href={"/profile"} style={{ color: "blue" }}>
         Go to Profile
@@ -16,12 +16,3 @@ const App = () => {
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
