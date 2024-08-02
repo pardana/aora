a350d7d5a4e2823569388496604022421273e04a
pardana <aditria.pardana@yahoo.co.id>
2024-08-02 14:09:47 +0700

    add: hide password

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,5 +1,6 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import React, { useState } from "react";
+import { icons } from "../constants";
 
 const FormField = ({
   title,
@@ -15,15 +16,25 @@ const FormField = ({
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
 
-      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center">
+      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
         <TextInput
-          className="flex-1 w-full text-white font-psemibold text-base"
+          className="flex-1 text-white font-psemibold text-base"
           value={value}
           placeholder={placeHolder}
           placeholderTextColor={"#7b7b8b"}
           onChangeText={(e) => handleChangeText(e)}
           secureTextEntry={title == "Password" ? !showPassword : false}
         />
+
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
